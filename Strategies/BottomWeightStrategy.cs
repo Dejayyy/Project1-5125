61122771969e239b46461c209f8c8598fe33c8d6
shrijalkc <shrijalkc@gmail.com>
2025-01-27 13:56:29 -0500

    Zeke added comments to bottomweightstrategy

--- Strategies/BottomWeightStrategy.cs
+++ Strategies/BottomWeightStrategy.cs
@@ -7,15 +7,17 @@ using System.Threading.Tasks;
 
 namespace Assi1.Strategies
 {
+    //BottomWeightStrategy class implements the IStackingStrategy interface
     public class BottomWeightStrategy : IStackingStrategy
     {
+        //EvaluateStack method that takes in a HeavyObjectList parameter called
         public float EvaluateStack(HeavyObjectList stack)
         {
-            float score = 0;
+            float score = 0; //declare float score with value 0
 
-            for (int i = 0; i < stack.Length(); i++)
+            for (int i = 0; i < stack.Length(); i++)//for loop to loop through the stack starting from the beginning of the stack
             {
-                score += stack.At(i).Mass / (i + 1);
+                score += stack.At(i).Mass / (i + 1);//Calculate score by adding Mass of every object divided by its position in the stack starting from bottom to top, start from 1 to avoid division by 0
             }
 
             return score;
